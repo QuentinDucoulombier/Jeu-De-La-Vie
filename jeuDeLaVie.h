7a9870778942c0f088cd1d996e74fc30fe609901
Quentin <83124122+Foraxfox@users.noreply.github.com>
2021-04-26 20:28:43 +0200

    Update jeuDeLaVie.h

--- jeuDeLaVie.h
+++ jeuDeLaVie.h
@@ -1,1 +1,10 @@
+#ifndef _jeuDeLaVie_H_
+#define _jeuDeLaVie_H_
 
+//void afficher(int tab, int largeur, int longueur);
+int** Allouer(int largeur, int longueur);
+void Initialisation(int** tab, int largeur, int longueur);
+void afficher(int** tab, int largeur, int longueur);
+void Libere (int** tab, int largeur, int longueur);
+
+#endif
