107d0d7b0f0140939e8fbefd8aa76cb1ea3122c4
g-pas2blaze <61205112+g-pas2blaze@users.noreply.github.com>
2021-04-27 10:59:53 +0200

    Update jeuDeLaVie.h

--- jeuDeLaVie.h
+++ jeuDeLaVie.h
@@ -6,5 +6,7 @@ int** Allouer(int largeur, int longueur);
 void Initialisation(int** tab, int largeur, int longueur);
 void afficher(int** tab, int largeur, int longueur);
 void Libere (int** tab, int largeur, int longueur);
+void copyTab(int** tab, int** tab2, int largeur, int longueur);
+void vivreEtMourir(int** tab, int** tab2, int largeur, int longueur);
 
 #endif
