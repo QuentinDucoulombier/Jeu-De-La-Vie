2ee9b4d974490e436eda1ad6d8795685296da589
Foraxfox <Foraxfox>
2021-05-18 14:35:11 +0200

    rajout de Dart.txt

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int main()
         for(int i = 0; i < nombreDeTours; i++)
         {
             copyMatrice(matrice2, matrice1, lignes, colonnes);
-            usleep(10000);
+            usleep(100000);
             printf("\033[2J");
             printf("\033[H");
             afficher(matrice1, lignes, colonnes);
